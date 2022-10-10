a09520a8c5c2603afe1ed4ef092ebc956a6333ed
Tom Moroney <moroneyt@tcd.ie>
2022-10-10 02:30:57 +0100

    Updated test file

--- src/test/java/calculatorTest.java
+++ src/test/java/calculatorTest.java
@@ -3,16 +3,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.DisplayName;
 
 public class calculatorTest {
-    public calculatorTest() {
-
-    }
 
     @Test
-    @DisplayName("Test the answer to everything")
-    public void testCalc() {
+    @DisplayName("Test Infix")
+    public void testInfix() {
         calculator calc = new calculator();
         assertEquals(5, calc.infixCalculator("2 + 3"));
         assertEquals(7, calc.infixCalculator("1 + 6"));
+        assertEquals(51, calc.infixCalculator("20 + 31"));
+        assertEquals(142, calc.infixCalculator("123 + 19"));
     }
 
 }
