4311d6c5a4cece20d4744327f98dbf6cfc0c1ce9
Tom Moroney <moroneyt@tcd.ie>
2022-10-10 01:24:15 +0100

    Added first test

--- src/test/java/calculatorTest.java
+++ src/test/java/calculatorTest.java
@@ -7,7 +7,7 @@ public class calculatorTest {
 
     @Test
     public void testCalc() {
-        assertEquals("2 3 + ", calc.inputInterpret("2 + 3"));
+        assertEquals(5, calc.infixCalculator("2 + 3"));
     }
 
 }
