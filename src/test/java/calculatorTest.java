371999547a91d0805fd338c21398286158b3955a
Tom Moroney <moroneyt@tcd.ie>
2022-10-10 02:34:46 +0100

    Added postfix test

--- src/test/java/calculatorTest.java
+++ src/test/java/calculatorTest.java
@@ -1,17 +1,24 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.DisplayName;
 
 public class calculatorTest {
 
     @Test
-    @DisplayName("Test Infix")
     public void testInfix() {
         calculator calc = new calculator();
         assertEquals(5, calc.infixCalculator("2 + 3"));
         assertEquals(7, calc.infixCalculator("1 + 6"));
         assertEquals(51, calc.infixCalculator("20 + 31"));
         assertEquals(142, calc.infixCalculator("123 + 19"));
+    }
+
+    @Test
+    public void testPostfix() {
+        calculator calc = new calculator();
+        assertEquals(5, calc.postfixCalculator("2 3 +"));
+        assertEquals(7, calc.postfixCalculator("1 6 +"));
+        assertEquals(51, calc.postfixCalculator("20 31 +"));
+        assertEquals(142, calc.postfixCalculator("123 19 +"));
     }
 
 }
